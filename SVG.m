12fced47481fcdb915aed701b23b3f5931c879db
b3m2a1 <b3m2a1@gmail.com>
2018-10-14 19:43:40 -0700

    I hate Mathematica

--- SVG.m
+++ SVG.m
@@ -25,6 +25,26 @@ Begin["`Private`"];
 
 
 (* ::Subsubsubsection::Closed:: *)
+(*toNotStupidString*)
+
+
+
+(* ::Text:: *)
+(*
+	ToString[x*10^7] is ridiculously stupid so we need to do this to fix it
+*)
+
+
+
+toNotStupidString[n_?IntegerQ]:=
+  StringTrim[Quiet@ToString@NumberForm[n, ExponentFunction->(Null&)], "."];
+toNotStupidString[n_?NumericQ]:=
+  StringTrim[Quiet@ToString@NumberForm[N@n, ExponentFunction->(Null&)], "."];
+toNotStupidString[e_]:=
+  ToString@e
+
+
+(* ::Subsubsubsection::Closed:: *)
 (*$SVGColorMap*)
 
 
@@ -56,7 +76,8 @@ $SVGValMap=
       (* other directly convertable values *)
       None->"none",
       Opacity[0]->"none",
-      Opacity[0.]->"none"
+      Opacity[0.]->"none",
+      Automatic->"auto"
       |>
     ]
 
@@ -96,11 +117,16 @@ $symbDashingFactors=
 
 
 
-toSVGString[RGBColor[r__], h_]:=
+toSVGString//Clear
+
+
+toSVGString[RGBColor[r__], h__]:=
   "#"<>IntegerString[Floor[{r}*255], 16, 2];
-toSVGString[h_?ColorQ, _]:=
-  toSVGString@ColorConvert[h, RGBColor];
-toSVGString[l_List, "stroke-dasharray"]:=
+toSVGString[h_?ColorQ, __]:=
+  toSVGString[ColorConvert[h, RGBColor], None, None];
+toSVGString[v_, _, "AbsoluteThickness"]:=
+  toSVGString[v, None, None]<>"pt";
+toSVGString[l_List, _, "Dashing"]:=
   StringRiffle@Map[ToString]@
     Module[
       {
@@ -116,27 +142,29 @@ toSVGString[l_List, "stroke-dasharray"]:=
         l
         ]
       ];
-toSVGString[h:Except[List][x_], _]:=(* handling things like Rotate *)
-  ToLowerCase@ToString[h]<>"("<>toSVGString[x]<>")";
-toSVGString[a_, head_]:=
+toSVGString[(h:Except[List])[x_], _, _]:=(* handling things like Rotate *)
+  ToLowerCase@ToString[h]<>"("<>toSVGString[x, None, None]<>")";
+toSVGString[a_, head_, _]:=
   Block[
     {
       $scaling=$symbScalingFactors
         (*If[head==="stroke-dasharray", $symbDashingFactors, $symbScalingFactors]*),
       vbmin=$svgViewBox[[3]](*Min[$svgViewBox[[3;;]]]*)
       },
-    Replace[
-      a//.{(* a bunch of junk to handle symbolic sizes *)
+    a//.{(* a bunch of junk to handle symbolic sizes *)
         k:Alternatives@@Keys[$scaling]:>
-          Lookup[$scaling, k]*vbmin,
-        {n_?NumericQ, m_?NumericQ}:>(ToString[n]<>","<>ToString[m])
-        },
-      {
+          Lookup[$scaling, k]*vbmin
+        }//ReplaceAll@{
+        {n_?NumericQ, m_?NumericQ}:>
+          (
+            toNotStupidString[n]<>","<>toNotStupidString[m]
+            ),
+        e:_?NumericQ:>toNotStupidString[e]
+        }//
+      Replace@{
         l_List:>StringRiffle[ToString/@l],
-        e:Except[_Integer, _?NumericQ]:>ToString@N[e],
         e_:>ToString@e
         }
-      ]
     ]
 
 
@@ -184,17 +212,20 @@ $SVGPropMap=
 
 
 
-decamel[s_]:=
-  StringTrim[
-    StringJoin@
-      StringSplit[s, l:LetterCharacter?(Not@*LowerCaseQ):>"-"<>ToLowerCase@l],
-    Except[LetterCharacter]
-    ]
+toSVGProp//Clear
+
+
+(*decamel[s_]:=
+	StringTrim[
+		StringJoin@
+			StringSplit[s, l:LetterCharacter?(Not@*LowerCaseQ)\[RuleDelayed]"-"<>ToLowerCase@l],
+		Except[LetterCharacter]
+		]*)
 
 
 toSVGProp[(form:EdgeForm|FaceForm|None)[key_], val_]:=
-  Module[{k, m},
-    k=ToString@key;
+  Module[{k1, k, m},
+    k1=k=ToString@key;
     m=
       Which[
         form===EdgeForm, 
@@ -204,22 +235,24 @@ toSVGProp[(form:EdgeForm|FaceForm|None)[key_], val_]:=
         True,
           $SVGPropMap
         ];
-    k=Lookup[m, k, Lookup[$SVGPropMap, k, If[StringQ@key, decamel[k], None]]];
+    k=Lookup[m, k, Lookup[$SVGPropMap, k, If[StringQ@key, k(*decamel[k]*), None]]];
     Which[
       k===None,
         Nothing,
       ListQ@k,
         MapThread[
-          #->Lookup[$SVGValMap, Key@#, toSVGString[val, #]]&,
+          #->Lookup[$SVGValMap, Key@#2, toSVGString[#2, #, k1]]&,
           {
             k,
             val
             }
           ],
       True,
-        k->Lookup[$SVGValMap, Key@val, toSVGString[val, k]]
+        k->Lookup[$SVGValMap, Key@val, toSVGString[val, k, k1]]
       ]
-    ]
+    ];
+toSVGProp[key_, val_]:=
+  toSVGProp[None@key, val]
 
 
 (* ::Subsubsubsection::Closed:: *)
@@ -227,8 +260,20 @@ toSVGProp[(form:EdgeForm|FaceForm|None)[key_], val_]:=
 
 
 
+toXML//Clear
+
+
 toXML[svgElement[head_, attrs_]]:=
-  XMLElement[head, KeyValueMap[toSVGProp, Association@attrs], {}]
+  XMLElement[head, 
+    DeleteDuplicatesBy[First]@KeyValueMap[toSVGProp,
+      KeyDrop[Association@attrs, "Body"]
+      ],
+    Flatten@List@ReplaceAll[
+      Lookup[attrs, "Body", {}],
+      s:(Alternatives@@Map[Blank, Prepend[Keys@$svgPrimitives, svgElement]]):>
+        toXML@toEl[s]
+      ]
+    ]
 
 
 (* ::Subsubsection::Closed:: *)
@@ -322,12 +367,16 @@ register[fn_, head_, args__]:=
 
 
 
-ClearAll@aliasedQ
+ClearAll@alias;
+ClearAll@aliasedQ;
+ClearAll@aliasTarget;
+ClearAll@lineTypeAlias;
 
 
-alias/:(alias[expr_]:=form_):=
-  (
+alias/:(alias[lineType:True|False][expr_]:=form_):=
+  Block[{$svgLineType=False},
     aliasedQ[expr]=True;
+    lineTypeAlias[expr]=lineType;
     aliasTarget[expr]=
       FirstCase[
         HoldComplete[form],
@@ -337,12 +386,18 @@ alias/:(alias[expr_]:=form_):=
         Heads->True
         ];
     toEl[expr]:=toEl[form]
-    )
+    ];
+alias/:(alias[expr_]:=form_):=
+  (alias[False][expr]:=form)
 
 
 (* ::Subsubsubsection::Closed:: *)
 (*Fallthrough*)
 
+
+
+toEl[e:_svgElement|_XMLElement]:=
+  e;
 
 
 toEl[e_]:=
@@ -394,7 +449,7 @@ register[svgLine, "line",
 
 
 
-register[svgPath, "path", {path_?StringQ->"d"}]
+register[svgPath, "path", path_?StringQ->"d"]
 
 
 (* ::Subsubsubsection::Closed:: *)
@@ -432,8 +487,36 @@ register[svgRect, "rect",
   ]
 
 
+(* ::Subsubsubsection::Closed:: *)
+(*marker*)
+
+
+
+register[svgMarker, "marker", id_?StringQ->"id", body_->"Body"]
+
+
+(* ::Subsubsubsection::Closed:: *)
+(*text*)
+
+
+
+register[svgText, "text", body_->"Body", {x_?NumericQ->"x", y_?NumericQ->"y"}]
+
+
 (* ::Subsubsection::Closed:: *)
 (*Aliases*)
+
+
+
+(* ::Subsubsubsection::Closed:: *)
+(*yAdjustCoord*)
+
+
+
+(* ::Text:: *)
+(*
+	Little function to get the coordinate system corrected
+*)
 
 
 
@@ -480,12 +563,10 @@ Disk[center_, rad:notOp[]:Automatic, ops:opsPat[]]:=
 
 
 
-alias@
+alias[True]@
 Circle[center_, rad:notOp[]:Automatic, ops:opsPat[]]:=
-  Block[{$svgLineType=True},
-    toEl[Disk[yAdjustCoord@center, rad, ops]]/.
-      svgCircle[a__]:>svgCircle[a, Filling->None]
-    ]
+  toEl[Disk[yAdjustCoord@center, rad, ops]]/.
+    svgElement["circle", a_]:>svgElement["circle", Append[a, FaceForm[Filling]->None]]
 
 
 (* ::Subsubsubsection::Closed:: *)
@@ -514,15 +595,87 @@ Rectangle[{xmin_, ymin_}, ops:opsPat[]]:=
 
 
 (* ::Subsubsubsection::Closed:: *)
-(*Line*)
+(*Polygon*)
 
 
 
 alias@
-Line[{pts:{_?NumericQ, _?NumericQ}..}, ops:opsPat[]]:=
-  Block[{$svgLineType=True},
-    svgLine[#, ops]&/@
-      yAdjustCoord@Partition[Riffle[Most@pts, Rest@pts], 2]
+Polygon[pts:{{_?NumericQ, _?NumericQ}..}, ops:opsPat[]]:=
+  svgPolygon[yAdjustCoord@pts, ops]
+
+
+alias@
+Polygon[poly:{{{_?NumericQ, _?NumericQ}..}..}, ops:opsPat[]]:=
+  svgPolygon[yAdjustCoord@#, ops]&/@poly
+
+
+(* ::Subsubsubsection::Closed:: *)
+(*Line*)
+
+
+
+alias[True]@
+Line[pts:{{_?NumericQ, _?NumericQ}, {_?NumericQ, _?NumericQ}}, ops:opsPat[]]:=
+  svgLine[yAdjustCoord@pts, ops]
+
+
+alias[True]@
+Line[pts:{{_?NumericQ, _?NumericQ}..}, ops:opsPat[]]:=
+  With[{dat=yAdjustCoord@pts},
+    svgPolyline[yAdjustCoord@pts, FaceForm[Filling]->None, ops]
+    ]
+
+
+(* ::Subsubsubsection::Closed:: *)
+(*Arrow*)
+
+
+
+arrowMarker//Clear
+
+
+arrowMarker[ops:opsPat[]]:=
+  Module[
+    {
+      heads=Lookup[ops, EdgeForm@Arrowheads, ops]
+      },
+    svgMarker["arrow-"<>StringTake[ToString@Hash@ops, 5],
+      svgPath@"M0,0 L0,6 L9,3 z",
+      "markerWidth"->10,
+      "markerHeight"->10,
+      "refX"->0,
+      "refY"->0,
+      "orient"->Automatic,
+      "markerUnits"->"strokeWidth",
+      ops
+      ]
+    ]
+
+
+alias[True]@
+Arrow[pts:{{_?NumericQ, _?NumericQ}..}, setBack:notOp[]:Automatic, ops:opsPat[]]:=
+  (* I'm ignoring setBack for now but I'll have to handle it some day... *)
+  With[{mark=arrowMarker[ops]},
+    Sow@mark;
+    Line[pts, ops, "marker"->URL[mark[[1]]]]
+    ]
+
+
+(* ::Subsubsubsection::Closed:: *)
+(*GraphicsComplex*)
+
+
+
+alias@
+g:GraphicsComplex[pts_, data_, o___]:=
+  toSVGCore[
+    Flatten@Replace[Normal@g,
+      gc_GraphicsComplex:>
+        With[{map=AssociationThread[Range[Length@pts], pts]},
+          data/.map (* this isn't actually going to be entirely clean, but it's good enough for now *)
+          ]
+      ],
+    {o}
     ]
 
 
@@ -531,9 +684,28 @@ Line[{pts:{_?NumericQ, _?NumericQ}..}, ops:opsPat[]]:=
 
 
 
+(* ::Text:: *)
+(*
+	This doesn\[CloseCurlyQuote]t actually work yet...
+*)
+
+
+
 alias@
 Rotate[g_, q_]:=
   Append[g, TransformationFunction->Rotate[q*Degree]]
+
+
+(* ::Subsubsubsection::Closed:: *)
+(*Annotation*)
+
+
+
+alias@
+Annotation[a_, d_, ___]:=
+  toSVGCore[a,
+    {"AnnotationData"->d}
+    ];
 
 
 (* ::Subsubsection::Closed:: *)
@@ -562,13 +734,18 @@ Module[
     opp=Flatten@{ops, Options[g]},
     imsize,
     viewbox,
-    prpad
+    pr,
+    prpad,
+    svgBody,
+    defs,
+    extra
     },
   imsize=
     4/3*Replace[OptionValue[Graphics, opp, ImageSize],
       {
         Automatic:>{360, 338},
-        n_?NumericQ:>{n, n}(* this isn't actually right, but we'll roll with it for now *)
+        n_?NumericQ:>
+          {n, Replace[OptionValue[Graphics, opp, AspectRatio], Except[_?NumericQ]->1]*n}
         }
       ];
   viewbox=Flatten@Transpose@PlotRange@g;
@@ -579,24 +756,41 @@ Module[
         Except[{_, _}]->{Scaled[.02], Scaled[.02]}
         }
       ];
-  viewbox=getViewBox[viewbox, prpad, imsize];
-  XMLElement["svg",
-    Flatten@{
-      Thread[
-        {"width","height"}->Map[ToString]@Floor@imsize
-        ],
-      "viewbox"->StringRiffle@Map[ToString]@viewbox,
-      "version"->"1.1",
-      {"http://www.w3.org/2000/xmlns/","xmlns"}->"http://www.w3.org/1999/xhtml"
+  {prpad, viewbox}=getViewBox[viewbox, prpad, imsize];
+  pr=PlotRange@g;
+  extra=axesAndFrame[opp, pr, prpad];
+  Block[
+    {
+      $svgPlotRange=pr,
+      $svgViewBox=viewbox,
+      $svgViewSize=imsize
       },
-    Block[
+    Quiet[
+      svgBody=Reap[toXML/@Flatten@toSVGCore[Throw@{extra, First@g}, opp]];
+      defs=Flatten@svgBody[[2]],
       {
-        $svgPlotRange=PlotRange@g,
-        $svgViewBox=viewbox,
-        $svgViewSize=imsize
+        (* optx messages don't matter here, so we quiet them if they show up *)
+        GraphicsComplex::optx,
+        GraphicsComplex::optrs
+        }
+      ];
+    XMLElement["svg",
+      Flatten@{
+        Thread[
+          {"width","height"}->Map[toNotStupidString]@Floor@imsize
+          ],
+        "viewbox"->StringRiffle@Map[toNotStupidString]@viewbox,
+        "version"->"1.1",
+        {"http://www.w3.org/2000/xmlns/","xmlns"}->"http://www.w3.org/1999/xhtml"
         },
-      toXML@exportGraphics[Join[Most[#], opp, {Last@#}]]&/@
-        splitGraphicsList[First@g]
+      If[Length@defs>0,
+        Prepend@XMLElement["defs",
+          {},
+          toXML@*toEl/@DeleteDuplicates@defs
+          ],
+        Identity
+        ]@
+        svgBody[[1]]
       ]
     ]
   ]
@@ -641,9 +835,168 @@ getViewBox[view_, pad_, size_]:=
         {x_, Scaled[y_]}:>
           {x, viewbox[[4]]*y}
         };
-    viewbox=
+    {
+      prpad,
       viewbox+Join[-prpad[[1, ;;2]], prpad[[1]]+prpad[[2]]]
+      }
     ]
+
+
+(* ::Subsubsubsection::Closed:: *)
+(*axesAndFrame*)
+
+
+
+getPlotTicksObjects[{{xmin_, xmax_}, {ymin_, ymax_}}, {x0_, y0_}]:=
+  Module[{xbase, ybase, ends},
+    xbase=Charting`ScaledTicks[{Identity,Identity}][xmin, xmax, 25];
+    ybase=Rest@Charting`ScaledTicks[{Identity,Identity}][ymin, ymax, 25];
+    {xbase, ybase}=
+      MapIndexed[
+        If[Mod[#2[[1]], 5]==4, 
+          ReplacePart[#, 3->{1.1, 1}*#[[3]]],
+          ReplacePart[#, 2->""]
+          ]&,
+        #
+        ]&/@{xbase, ybase};
+    {
+      Function[
+        ends={{#[[1]], y0-#[[3, 2]]}, {#, y0+#[[3, 1]]}};
+        {#[[-1]], If[#[[2]]=!="", Text[#[[2]], ends[[1]]], Nothing], Line[ends]}
+        ]/@xbase,
+      Function[
+        ends={{x0-#[[3, 2]], #[[1]]}, {x0+#[[3, 1]], #}};
+        {#[[-1]], If[#[[2]]=!="", Text[#[[2]], ends[[1]]], Nothing], Line[ends]}
+        ]/@ybase
+      }
+    ]
+
+
+axesAndFrame[opp_, prange_, pad_]:=
+  Module[
+    {
+      pr=prange+pad,
+      axes,
+      aticks,
+      ast,
+      orgy,
+      axesLines,
+      fst,
+      frame,
+      frameLines,
+      axesTicks,
+      frameTicks
+      },
+    axes=
+      Replace[OptionValue[Graphics, opp, Axes], 
+        {
+          Automatic->{False, False},
+          e:Except[{_, _}]:>{e, e}
+          }
+        ];
+    orgy=
+      Replace[OptionValue[Graphics, opp, AxesOrigin], 
+        {
+          e:Except[{_, _}]:>{prange[[1, 1]], prange[[2, 1]]}
+          }
+        ];
+    ast=Flatten@OptionValue[Graphics, opp, AxesStyle];
+    axesLines=
+      If[Or@@Map[TrueQ, axes],
+        {
+          ast,
+          If[TrueQ@axes[[1]],
+            Line[Thread[{pr[[1]], orgy[[2]]}]],
+            Nothing
+            ],
+          If[TrueQ@axes[[2]],
+            Line[Thread[{orgy[[1]], pr[[2]]}]],
+            Nothing
+            ]
+          },
+        Nothing
+        ];
+    aticks=
+      Replace[OptionValue[Graphics, opp, Ticks], 
+        {
+          Automatic->{axes[[1]], axes[[2]]},
+          e:Except[{_, _}]:>{e, e}
+          }
+        ];
+    aticks[[1]]=TrueQ[aticks[[1]]]||(aticks[[1]]===Automatic&&axes[[1]]);
+    aticks[[2]]=TrueQ[aticks[[2]]]||(aticks[[2]]===Automatic&&axes[[2]]);
+    axesTicks=
+      {
+        If[aticks[[1]], #[[1]], Nothing],
+        If[aticks[[2]], #[[2]], Nothing]
+        }&@getPlotTicksObjects[pr, orgy];
+    frame=
+      Replace[OptionValue[Graphics, opp, Frame], 
+        {
+          Automatic->
+            {
+              {False, False},
+              {False, False}
+              },
+          f:{{_, _}, {_, _}}:>f,
+          {a_, b_}:>
+            {{a, b}, {a, b}},
+          e:Except[{_, _}]:>{{e, e}, {e, e}}
+          }
+        ];
+    fst=
+      Replace[OptionValue[Graphics, opp, FrameStyle], 
+        {
+          Automatic->
+            {
+              {{}, {}},
+              {{}, {}}
+              },
+          f:{{_, _}, {_, _}}:>f,
+          {a_, b_}:>
+            {{a, b}, {a, b}},
+          e:Except[{_, _}]:>{{e, e}, {e, e}}
+          }
+        ];
+    frameLines=
+      If[Or@@Map[TrueQ, Flatten@frame],
+        {
+          If[TrueQ@frame[[1, 1]]&&fst[[1, 1]]=!=None,
+            {fst[[1, 1]], Line@Thread[{pr[[1, 1]], pr[[2]]}]},
+            Nothing
+            ],
+          If[TrueQ@frame[[1, 2]]&&fst[[1, 2]]=!=None,
+            {fst[[1, 2]], Line@Thread[{pr[[1]], pr[[2, 1]]}]},
+            Nothing
+            ],
+          If[TrueQ@frame[[2, 1]]&&fst[[2, 1]]=!=None,
+            {fst[[2, 1]], Line@Thread[{pr[[1, 2]], pr[[2]]}]},
+            Nothing
+            ],
+          If[TrueQ@frame[[2, 2]]&&fst[[2, 2]]=!=None,
+            {fst[[2, 2]], Line@Thread[{pr[[1]], pr[[2, 2]]}]},
+            Nothing
+            ]
+          },
+        Nothing
+        ];
+    {
+      axesLines,
+      axesTicks,
+      frameLines
+      (* need to do the FrameTicks stuff *)
+      }
+    ]
+
+
+(* ::Subsubsubsection::Closed:: *)
+(*toSVGCore*)
+
+
+
+toSVGCore[e_, opp_]:=
+  exportGraphics[Join[Most[#], opp, {Last@#}]]&/@
+    splitGraphicsList[e]
 
 
 (* ::Subsubsubsection::Closed:: *)
@@ -655,17 +1008,15 @@ exportGraphics//Clear
 
 
 exportGraphics[{dir___, expr_}, ops:OptionsPattern[]]:=
-  Block[{$svgLineType=False},
-    toEl@
-      Insert[
-        expr,
-        Flatten@{
-          canonicalizeDirectives[{dir}, expr],
-          ops
-          },
-        -1
-        ]
-    ]
+  toEl@
+    Insert[
+      expr,
+      Flatten@{
+        canonicalizeDirectives[{dir}, expr],
+        ops
+        },
+      -1
+      ];
 
 
 (* ::Subsubsubsection::Closed:: *)
@@ -691,8 +1042,12 @@ canonicalizeDirectives//Clear
 canonicalizeDirectives[{}, expr_]:=
   {};
 canonicalizeDirectives[dir_, expr_]:=
-  With[{targ=aliasTarget[expr]},
-    Map[
+  Block[
+    {
+      targ=aliasTarget[expr],
+      $svgLineType=TrueQ@lineTypeAlias[expr]
+      },
+    DeleteDuplicatesBy[First]@Map[
       canonicalizeDirective[#, targ]&,
       {dir}//.
         {
@@ -717,11 +1072,6 @@ canonicalizeDirective[c_?ColorQ, _]:=
     Evaluate[EdgeForm[Filling]->c],
     Evaluate[FaceForm[Filling]->c]
     ];
-canonicalizeDirective[head_[x_], _]:=
-  If[$svgLineType, 
-    Evaluate[EdgeForm[head]->x],
-    Evaluate[FaceForm[head]->x]
-    ];
 canonicalizeDirective[FaceForm[x_], targ_]:=
   Block[{$svgLineType=False},
     canonicalizeDirective[x, targ]
@@ -732,8 +1082,26 @@ canonicalizeDirective[EdgeForm[x_], targ_]:=
     ];
 canonicalizeDirective[r:_Rule|_RuleDelayed, _]:=
   r;
+canonicalizeDirective[(head:Except[List])[x_, ___], _]:=
+  If[$svgLineType, 
+    Evaluate[EdgeForm[head]->x],
+    Evaluate[FaceForm[head]->x]
+    ];
 canonicalizeDirective[e___]:=
-  (Echo@{e};Nothing)
+  (Nothing)
+
+
+(* ::Subsubsubsection::Closed:: *)
+(*$defaultDirectives*)
+
+
+
+$defaultDirectives=
+  {
+    EdgeForm@
+      Directive[Black, Thickness[.005]],
+    FaceForm[Black]
+    }
 
 
 (* ::Subsubsubsection::Closed:: *)
@@ -743,7 +1111,7 @@ canonicalizeDirective[e___]:=
 
 splitGraphicsListRec[gl_]:=
   Block[
-    {direct=If[!ListQ@direct, {}, direct]},
+    {direct=direct},
     Which[
       ListQ@#, 
         splitGraphicsListRec[#],
@@ -756,7 +1124,9 @@ splitGraphicsListRec[gl_]:=
 
 
 splitGraphicsList[gl_]:=
-  Flatten[Reap[splitGraphicsListRec[gl];][[2]], 1]
+  Block[{direct=$defaultDirectives},
+    Flatten[Reap[splitGraphicsListRec[gl];][[2]], 1]
+    ]
 
 
 End[];
